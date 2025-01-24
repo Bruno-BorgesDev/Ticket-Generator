cd9daeec2ad113690f8a04b85a7a3ba393c1712c
Ivan de Oliveira <ivan.oliveira.pe@outlook.com>
2025-01-24 19:49:08 -0300

    Validação de email ajustada para que a mensagem de erro apareça antes de clicar no botão.

--- js/validacaoDeEmail.js
+++ js/validacaoDeEmail.js
@@ -1,10 +1,8 @@
-const campoEmailEvento = document.querySelector("button");
-
+const campoEmailEvento = document.getElementById("email");
 function verificadorEmail(email) {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
 }
-
 function validarEmail() {
   const campoEmail = document.getElementById("email").value;
   if (!verificadorEmail(campoEmail)) {
@@ -13,5 +11,4 @@ function validarEmail() {
     document.getElementById("erro-email-invalido").style.display = "none";
   }
 }
-
-campoEmailEvento.addEventListener("click", validarEmail);
+campoEmailEvento.addEventListener("blur", validarEmail);
